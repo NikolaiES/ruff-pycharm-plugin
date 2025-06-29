05883079251bea97fd652e4df398a92ce536efa8
Nikolai Eugen Sandvik <nikolai-git@sandvik.dev>
2025-06-29 08:19:07 +0200

    Added support for pulling diagnostics

--- src/com/koxudaxi/ruff/lsp/intellij/supports/RuffLspDiagnosticsSupport.kt
+++ src/com/koxudaxi/ruff/lsp/intellij/supports/RuffLspDiagnosticsSupport.kt
@@ -1,8 +1,11 @@
 package com.koxudaxi.ruff.lsp.intellij.supports
 
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.platform.lsp.api.customization.LspDiagnosticsSupport
 
 
 @Suppress("UnstableApiUsage")
-class RuffLspDiagnosticsSupport(val project: Project) : LspDiagnosticsSupport()
+class RuffLspDiagnosticsSupport(val project: Project) : LspDiagnosticsSupport() {
+    override fun shouldAskServerForDiagnostics(file: VirtualFile): Boolean = true
+}
